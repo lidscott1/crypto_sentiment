da20570d2f05aa8d1c4fbce992426be74a9ee097
lidscott1 <lidscott@gmail.com>
2018-02-10 14:32:30 -0600

    deleting old code

--- api_request_script.py
+++ api_request_script.py
@@ -117,85 +117,27 @@ class ApiCall(object):
 
 
 
-test = ApiCall("BTC OR bitcoin", "2018-01-01", 
-               "2018-01-02", "54826b7dfe0845f5bcda79a02538c1db")
+#test = ApiCall("BTC OR bitcoin", "2018-01-01", 
+#               "2018-01-02", "54826b7dfe0845f5bcda79a02538c1db")
 
+
+##test examples
 
 ##expecting 10 results
-test.get_data()
+#test.get_data()
 
 ##expecting 5
-test.get_data(1, 5)
+#test.get_data(1, 5)
 
 ##expecting 1
-test.get_data(5)
+#test.get_data(5)
 
 ##expecting 5
-test.get_data(None, 5)
+#test.get_data(None, 5)
 
 ##expecting assert error
-test.get_data(5, 4)
+#test.get_data(5, 4)
 
-
-class Struct(object):
-
-    def __init__(self, data):
-        for name, value in data.items():
-            setattr(self, name, self._wrap(value))
-
-    def _wrap(self, value):
-        if isinstance(value, (tuple, list, set, frozenset)): 
-            return type(value)([self._wrap(v) for v in value])
-        else:
-            return Struct(value) if isinstance(value, dict) else value
-
-class myclass(object):
-
-    def get_raw_data(self,call):
-        r = requests.get(call).json()
-        
-        for i in r['articles']:
-            del i['author']
-            del i['urlToImage']
-            t = datetime.datetime.strptime(i['publishedAt'], "%Y-%m-%dT%H:%M:%S%fZ")
-            nt = t.replace(hour=0, minute=0, second=0, microsecond=0)
-            i['publishedAt'] = str(nt)
-            i['source'] = i['source']['name']
-        
-        return r
-    
-    def __init__(self,call):
-        self.call = call
-        self.data = Struct(self.get_raw_data(call))
-        self.data.n_pages = self.data.totalResults/20
-        
-    def paginate(self,n):
-        fp = self.call.find('&page=')
-        if fp > 0:
-            l = [x for x, v in enumerate(self.call) if v == '&']
-            l.append(len(self.call))
-            nxt = l[next(x[0] for x in enumerate(l) if x[1] > fp)]
-            base_call = self.call[:fp] + self.call[nxt:]
-        else:
-            base_call = self.call
-         
-        articles_list = []
-
-        for i in range(1,n+1):
-            new_call = base_call + "&page=" + str(i)
-            d = Struct(self.get_raw_data(new_call))
-            articles_list.extend(d.articles)
-            
-        return articles_list
-
-n =  myclass(sys.argv[1])
-
-if n.data.n_pages > 900:
-    p = 900
-else:
-    p = int(n.data.n_pages)
-
-l = n.paginate(p)
 
 with open(sys.argv[2], "wb") as f:
     pickle.dump(l, f)
